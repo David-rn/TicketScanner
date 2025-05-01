a9d9aa68bece236fc0725f030980665dbf82d83a
David Redo <dredonieto@gmail.com>
2025-05-01 19:16:40 +0200

    Added main file

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    from doctr.io import DocumentFile
+    from doctr.models import ocr_predictor
+
+    model = ocr_predictor(pretrained=True)
+    # PDF
+    doc = DocumentFile.from_images("/Users/dredo/Downloads/pizzeria.jpg")
+    # Analyze
+    result = model(doc)
+    print(result)
+    result.show()
+
+
+if __name__ == "__main__":
+    main()
